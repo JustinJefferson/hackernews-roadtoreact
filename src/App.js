3ac839303fb0090d89cb2f2bc36298b775b899e7
JustinJefferson <justleland@gmail.com>
2020-02-01 15:52:19 -0500

    Learning props and local state management

--- src/App.js
+++ src/App.js
@@ -1,13 +1,85 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class Developer {
+  constructor(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+
+  getName() {
+    return this.firstname + ' ' + this.lastname;
+  }
+}
+
+const helloWorld = 'Time to Start Reacting!';
+
+const robin = new Developer('Robin', 'Thorn');
+const george = new Developer('George', 'Takei');
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1
+  }
+];
+
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      list,
+      helloWorld,
+      robin,
+      george
+    }
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const updatedList = this.state.list.filter(function isNotId(item) {
+      return item.objectID !== id;
+    });
+    this.setState({list: updatedList});
+  }
+
   render () {
-    const helloWorld = 'Time to Start Reacting!';
+    
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        <h2>{this.state.helloWorld}</h2>
         <p>The world is so reactable!</p>
+        {this.state.list.map(item => 
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span> by {item.author},</span>
+            <span> comments: {item.num_comments}</span>
+            <span> score: {item.points} </span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                Dismiss
+              </button>
+            </span>
+          </div>
+        )}
+        <p>Hello my name is {this.state.robin.getName()}. I am a software developer.</p>
+        <p>Ohayou, Boku wa {this.state.george.getName()} desu. Shigoto wa compyutaa desu.</p>
       </div>
     );
   }
